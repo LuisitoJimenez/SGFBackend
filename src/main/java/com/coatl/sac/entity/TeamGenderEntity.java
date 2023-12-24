98b2ac81b3d739b1f5889a7d3b8d67484d70b975
Luis Jimenez <wichin@outlook.com>
2023-12-24 02:17:00 -0600

    Add TeamGenderEntity class

---
+++ src/main/java/com/coatl/sac/entity/TeamGenderEntity.java
@@ -0,0 +1,55 @@
+package com.coatl.sac.entity;
+
+import java.sql.Timestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "teams_genders")
+public class TeamGenderEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true)
+    private Integer id;
+
+    @Basic
+    @Column(name = "team_id", nullable = false)
+    private Integer teamId;
+
+    @Basic
+    @Column(name = "gender_id", nullable = false)
+    private Integer genderId;
+
+    @Basic
+    @Column(name = "user_created")
+    @JsonIgnore
+    private Integer userCreated;
+    
+    @Basic
+    @Column(name = "created")
+    @JsonIgnore
+    private Timestamp created;
+
+    @Basic
+    @Column(name = "user_deleted")
+    @JsonIgnore
+    private Integer userDeleted;
+
+    @Basic
+    @Column(name = "deleted")
+    @JsonIgnore
+    private Timestamp deleted;
+}
