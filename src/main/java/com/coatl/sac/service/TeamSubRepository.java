ddb03f99fbfaccb26d24cc736952a2c811fa92d0
Luis Jimenez <wichin@outlook.com>
2023-12-24 02:17:15 -0600

    Add findByTeamIdAndDeletedIsNull method to TeamSubRepository

--- src/main/java/com/coatl/sac/service/TeamSubRepository.java
+++ src/main/java/com/coatl/sac/service/TeamSubRepository.java
@@ -1,5 +1,7 @@
 package com.coatl.sac.service;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.coatl.sac.entity.TeamSubEntity;
 @Repository
 public interface TeamSubRepository extends JpaRepository<TeamSubEntity, Integer>{
 
+    Optional<TeamSubEntity> findByTeamIdAndDeletedIsNull(Integer teamId);
 }
