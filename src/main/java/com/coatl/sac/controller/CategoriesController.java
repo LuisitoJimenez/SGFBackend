2d9d43fa4c17fa0a65456510fea8f60bfb2aad85
Luis Jimenez <wichin@outlook.com>
2023-12-23 23:35:51 -0600

    Add CategoriesController with getCategoriesByAge endpoint

---
+++ src/main/java/com/coatl/sac/controller/CategoriesController.java
@@ -0,0 +1,29 @@
+package com.coatl.sac.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.coatl.sac.dto.WebServiceResponse;
+import com.coatl.sac.service.CategoriesService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/categories")
+@Tag(name = "Categories", description = "Categories management")
+public class CategoriesController {
+
+    @Autowired
+    private CategoriesService categoriesService;
+
+    @GetMapping("/subs")
+    @Operation(summary = "Get categories by age")
+    public WebServiceResponse getCategoriesByAge() {
+        return new WebServiceResponse(categoriesService.getSubList());
+    }
+}
