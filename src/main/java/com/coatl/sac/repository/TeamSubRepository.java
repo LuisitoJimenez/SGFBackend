9dfee97422cc5499c44ec16b70cf6aa19ba042ec
Luis Jimenez <wichin@outlook.com>
2023-12-26 21:26:59 -0600

    Add TeamSubRepository interface

---
+++ src/main/java/com/coatl/sac/repository/TeamSubRepository.java
@@ -0,0 +1,16 @@
+package com.coatl.sac.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.coatl.sac.entity.TeamSubEntity;
+
+@Repository
+public interface TeamSubRepository extends JpaRepository<TeamSubEntity, Integer>{
+
+    Optional<TeamSubEntity> findByTeamIdAndDeletedIsNull(Integer teamId);
+
+    //Optional<TeamSubEntity> findByTeamIdAndDeletedIsNull(Integer teamId);
+}
