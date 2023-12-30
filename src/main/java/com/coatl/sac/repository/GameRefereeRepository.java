fc1e1271ee48232ca1d5393d57161faae86fe895
Luis Jimenez <wichin@outlook.com>
2023-12-29 23:17:52 -0600

    Create repository

---
+++ src/main/java/com/coatl/sac/repository/GameRefereeRepository.java
@@ -0,0 +1,16 @@
+package com.coatl.sac.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.coatl.sac.entity.GameRefereeEntity;
+
+@Repository
+public interface GameRefereeRepository extends JpaRepository<GameRefereeEntity, Integer> {
+    
+    Optional<GameRefereeEntity> findByGameId(Integer gameId);
+
+    Optional<GameRefereeEntity> findByGameIdAndDeletedIsNull(Integer gameId);
+}
