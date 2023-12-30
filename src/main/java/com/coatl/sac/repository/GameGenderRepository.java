bfa5c190617e9f93bcc964ee80f6646daea16760
Luis Jimenez <wichin@outlook.com>
2023-12-29 23:17:17 -0600

    Add new methods to the repository

--- src/main/java/com/coatl/sac/repository/GameGenderRepository.java
+++ src/main/java/com/coatl/sac/repository/GameGenderRepository.java
@@ -1,11 +1,21 @@
 package com.coatl.sac.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.coatl.sac.entity.GameGenderEntity;
+import com.coatl.sac.entity.TeamGenderEntity;
 
 @Repository
 public interface GameGenderRepository extends JpaRepository<GameGenderEntity, Integer>{
     
+    //Optional<GameGenderEntity> findByGameId(Integer gameId);
+
+    Optional<GameGenderEntity> findByGameIdAndDeletedIsNull(Integer gameId);
+
+    Optional<GameGenderEntity> findByGameIdAndGenderId(Integer gameId, Integer genderId);
+
+    Optional<GameGenderEntity> findByGameId(Integer gameId);
 }
