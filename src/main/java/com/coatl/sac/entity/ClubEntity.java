be3fa1289f27ba1fdfd12d86962c48fad4f4b468
Luis Jimenez <wichin@outlook.com>
2024-01-04 01:09:48 -0600

    Refactor ClubEntity class by extending BaseEntity

--- src/main/java/com/coatl/sac/entity/ClubEntity.java
+++ src/main/java/com/coatl/sac/entity/ClubEntity.java
@@ -1,9 +1,8 @@
 package com.coatl.sac.entity;
 
-import java.sql.Timestamp;
-
 import org.hibernate.annotations.Type;
 
+import com.coatl.sac.entity.base.BaseEntity;
 import com.coatl.sac.json.SocialNetwork;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -12,9 +11,6 @@ import io.hypersistence.utils.hibernate.type.json.JsonType;
 import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,13 +18,8 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "clubs")
-public class ClubEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false, unique = true)
-    private Integer id;
+@Table(name = "club")
+public class ClubEntity extends BaseEntity{
 
     @Basic
     @Column(name = "logo", columnDefinition = "longtext")
@@ -53,23 +44,4 @@ public class ClubEntity {
     @Column(name = "phone", nullable = false)
     private String phone;
 
-    @Basic
-    @Column(name = "user_created")
-    @JsonIgnore
-    private Integer userCreated;
-
-    @Basic
-    @Column(name = "created")
-    @JsonIgnore
-    private Timestamp created;
-
-    @Basic
-    @Column(name = "user_deleted")
-    @JsonIgnore
-    private Integer userDeleted;
-
-    @Basic
-    @Column(name = "deleted")
-    @JsonIgnore
-    private Timestamp deleted;
 }
