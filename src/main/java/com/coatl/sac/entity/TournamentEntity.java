94353b56da2d7075b5c34b7a2e575832c29326e9
Luis Jimenez <wichin@outlook.com>
2024-01-03 23:48:59 -0600

    Refactor TournamentEntity class

--- src/main/java/com/coatl/sac/entity/TournamentEntity.java
+++ src/main/java/com/coatl/sac/entity/TournamentEntity.java
@@ -1,15 +1,9 @@
 package com.coatl.sac.entity;
 
-import java.sql.Timestamp;
+import com.coatl.sac.entity.base.BaseEntity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,52 +11,17 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "tournaments")
-public class TournamentEntity {
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false, unique = true)
-    private Integer id;
+@Table(name = "tournament")
+public class TournamentEntity extends BaseEntity{
 
-    @Basic
+    @Column(name = "logo")
+    private String logo;
+
     @Column(name = "name", nullable = false)
     private String name;
 
-    @Basic
-    @Column(name = "start_date", nullable = false)
-    private Timestamp startDate;
-
-    @Basic
-    @Column(name = "end_date", nullable = false)
-    private Timestamp endDate;
-
-    /* @Basic
-    @Column(name = "age_category", nullable = false)
-    private String ageCategory; */
-
-    /* @Basic
-    @Column(name = "gender_category", nullable = false)
-    private String genderCategory; */
-
-    @Basic
-    @Column(name = "user_created")
-    @JsonIgnore
-    private Integer userCreated;
+    @Column(name = "description")
+    private String description;
     
-    @Basic
-    @Column(name = "created")
-    @JsonIgnore
-    private Timestamp created;
 
-    @Basic
-    @Column(name = "user_deleted")
-    @JsonIgnore
-    private Integer userDeleted;
-
-    @Basic
-    @Column(name = "deleted")
-    @JsonIgnore
-    private Timestamp deleted;
-    
 }
