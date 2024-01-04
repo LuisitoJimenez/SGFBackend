0e461166d8e5aa1b0cfba98e6c6e1bc4531369f0
Luis Jimenez <wichin@outlook.com>
2024-01-04 01:11:12 -0600

    Refactor TeamPlayerEntity class to extend BaseEntity

--- src/main/java/com/coatl/sac/entity/TeamPlayerEntity.java
+++ src/main/java/com/coatl/sac/entity/TeamPlayerEntity.java
@@ -1,15 +1,10 @@
 package com.coatl.sac.entity;
 
-import java.sql.Timestamp;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.coatl.sac.entity.base.BaseEntity;
 
 import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,13 +12,8 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "teams_players")
-public class TeamPlayerEntity {
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false, unique = true)
-    private Integer id;
+@Table(name = "team_player")
+public class TeamPlayerEntity extends BaseEntity{
 
     @Basic
     @Column(name = "team_id", nullable = false)
@@ -41,23 +31,4 @@ public class TeamPlayerEntity {
     @Column(name = "type_player_id", nullable = false)
     private Integer typePlayerId;
 
-    @Basic
-    @Column(name = "user_created")
-    @JsonIgnore
-    private Integer userCreated;
-    
-    @Basic
-    @Column(name = "created")
-    @JsonIgnore
-    private Timestamp created;
-
-    @Basic
-    @Column(name = "user_deleted")
-    @JsonIgnore
-    private Integer userDeleted;
-
-    @Basic
-    @Column(name = "deleted")
-    @JsonIgnore
-    private Timestamp deleted;
 }
