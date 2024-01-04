a80f993e5b1e42f3ad0e3913f5a120f14ac1fc17
Luis Jimenez <wichin@outlook.com>
2024-01-04 01:10:27 -0600

    Refactor PlayerEntity class

--- src/main/java/com/coatl/sac/entity/PlayerEntity.java
+++ src/main/java/com/coatl/sac/entity/PlayerEntity.java
@@ -1,10 +1,10 @@
 package com.coatl.sac.entity;
 
 import java.sql.Date;
-import java.sql.Timestamp;
 
 import org.hibernate.annotations.Type;
 
+import com.coatl.sac.entity.base.BaseEntity;
 import com.coatl.sac.json.Birthplace;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -13,9 +13,6 @@ import io.hypersistence.utils.hibernate.type.json.JsonType;
 import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -23,13 +20,8 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "players")
-public class PlayerEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
-    private Integer id;
+@Table(name = "player")
+public class PlayerEntity extends BaseEntity{
 
     @Basic
     @JsonIgnore
@@ -62,28 +54,4 @@ public class PlayerEntity {
     @Column (name = "height")
     private Float height;
 
-/*     @Basic
-    @JsonIgnore
-    @Column(name = "nationality")
-    private String nationality; */
-
-    @Basic
-    @JsonIgnore
-    @Column(name = "user_created")
-    private Integer userCreated;
-
-    @Basic
-    @JsonIgnore
-    @Column(name = "created")
-    private Timestamp created;
-
-    @Basic
-    @JsonIgnore
-    @Column(name = "user_deleted")
-    private Integer userDeleted;
-
-    @Basic
-    @JsonIgnore
-    @Column(name = "deleted")
-    private Timestamp deleted;
 }
