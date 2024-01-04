64572cdddd7b28e094abb6e947d219e70fc88381
Luis Jimenez <wichin@outlook.com>
2024-01-04 01:10:06 -0600

    Refactor GameGenderEntity class

--- src/main/java/com/coatl/sac/entity/GameGenderEntity.java
+++ src/main/java/com/coatl/sac/entity/GameGenderEntity.java
@@ -1,15 +1,10 @@
 package com.coatl.sac.entity;
 
-import java.sql.Timestamp;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.coatl.sac.entity.base.BaseEntity;
 
 import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,13 +12,8 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "games_genders")
-public class GameGenderEntity {
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
-    private Integer id;
+@Table(name = "game_gender")
+public class GameGenderEntity extends BaseEntity {
 
     @Basic
     @Column(name = "game_id", nullable = false)
@@ -33,24 +23,4 @@ public class GameGenderEntity {
     @Column(name = "gender_id", nullable = false)
     private Integer genderId;
  
-    @Basic
-    @Column(name = "user_created")
-    @JsonIgnore
-    private Integer userCreated;
-    
-    @Basic
-    @Column(name = "created")
-    @JsonIgnore
-    private Timestamp created;
-
-    @Basic
-    @Column(name = "user_deleted")
-    @JsonIgnore
-    private Integer userDeleted;
-
-    @Basic
-    @Column(name = "deleted")
-    @JsonIgnore
-    private Timestamp deleted;
-
 }
