7cc806593e25b4afba74ef8f976c8fe6f005eac9
Luis Jimenez <wichin@outlook.com>
2024-01-04 01:11:21 -0600

    Refactor UserEntity class by extending BaseEntity

--- src/main/java/com/coatl/sac/entity/UserEntity.java
+++ src/main/java/com/coatl/sac/entity/UserEntity.java
@@ -1,11 +1,9 @@
 package com.coatl.sac.entity;
 
-import java.sql.Timestamp;
-import java.util.List;
-
 import org.hibernate.annotations.NaturalId;
 import org.hibernate.annotations.Type;
 
+import com.coatl.sac.entity.base.BaseEntity;
 import com.coatl.sac.json.UserName;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -18,13 +16,8 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "users")
-public class UserEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false, unique = true)
-    private Integer id;
+@Table(name = "user")
+public class UserEntity extends BaseEntity{
 
     @NaturalId
     @JsonIgnoreProperties(ignoreUnknown = true)
@@ -46,43 +39,4 @@ public class UserEntity {
     @Column(name = "phone", nullable = false)
     private String phone;
 
-    /* @Basic
-    @Column(name = "birthday")
-    @JsonIgnore
-    private Date birthday; */
-
-    /* @Type(JsonType.class)
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    @Column(name = "address", columnDefinition = "longtext")
-    private Address address;
- */
-    @Basic
-    @Column(name = "user_created")
-    @JsonIgnore
-    private Integer userCreated;
-    
-    @Basic
-    @Column(name = "created")
-    @JsonIgnore
-    private Timestamp created;
-
-    @Basic
-    @Column(name = "user_deleted")
-    @JsonIgnore
-    private Integer userDeleted;
-
-    @Basic
-    @Column(name = "deleted")
-    @JsonIgnore
-    private Timestamp deleted;
-
-    @JsonIgnore
-    @ManyToMany
-    @JoinTable(
-        name = "users_teams",
-        joinColumns = @JoinColumn(name = "user_id"),
-        inverseJoinColumns = @JoinColumn(name = "team_id")
-    )
-    private List<TeamEntity> teams;
-    
 }
