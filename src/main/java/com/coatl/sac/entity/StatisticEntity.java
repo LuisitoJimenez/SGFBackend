4aa06e01db6a729e82156af6fa147cb86dd453bf
Luis Jimenez <wichin@outlook.com>
2024-01-04 01:10:51 -0600

    Remove unused imports in StatisticEntity.java

--- src/main/java/com/coatl/sac/entity/StatisticEntity.java
+++ src/main/java/com/coatl/sac/entity/StatisticEntity.java
@@ -2,12 +2,8 @@ package com.coatl.sac.entity;
 
 import com.coatl.sac.entity.base.BaseEntity;
 
-import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
