248c16ce6ef3df2123cf37d0f5c7854166351852
Luis Jimenez <wichin@outlook.com>
2024-01-04 01:10:38 -0600

    Refactor RefereeGenderEntity class

--- src/main/java/com/coatl/sac/entity/RefereeGenderEntity.java
+++ src/main/java/com/coatl/sac/entity/RefereeGenderEntity.java
@@ -1,13 +1,10 @@
 package com.coatl.sac.entity;
 
-import java.sql.Timestamp;
+import com.coatl.sac.entity.base.BaseEntity;
 
 import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,13 +12,8 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "referees_genders")
-public class RefereeGenderEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false, unique = true)
-    private Integer id;
+@Table(name = "referee_gender")
+public class RefereeGenderEntity extends BaseEntity {
 
     @Basic
     @Column(name = "referee_id", nullable = false)
@@ -31,19 +23,4 @@ public class RefereeGenderEntity {
     @Column(name = "gender_id")
     private Integer genderId;
 
-    @Basic
-    @Column(name = "user_created", nullable = false)
-    private Integer userCreated;
-
-    @Basic
-    @Column(name = "created")
-    private Timestamp created;
-
-    @Basic
-    @Column(name = "user_deleted")
-    private Integer userDeleted;
-
-    @Basic
-    @Column(name = "deleted")
-    private Timestamp deleted;
 }
