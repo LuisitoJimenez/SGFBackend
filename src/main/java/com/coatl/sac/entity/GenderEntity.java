5ceac49eb1daea7efc35839ee9a4615c0885be17
Luis Jimenez <wichin@outlook.com>
2024-01-04 01:10:23 -0600

    Refactor GenderEntity class to extend BaseEntity

--- src/main/java/com/coatl/sac/entity/GenderEntity.java
+++ src/main/java/com/coatl/sac/entity/GenderEntity.java
@@ -1,15 +1,10 @@
 package com.coatl.sac.entity;
 
-import java.sql.Timestamp;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.coatl.sac.entity.base.BaseEntity;
 
 import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,39 +12,11 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "genders")
-public class GenderEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false, unique = true)
-    private Integer id;
+@Table(name = "gender")
+public class GenderEntity extends BaseEntity{
 
     @Basic
     @Column(name = "name", nullable = false)
     private String name;
 
-    @Basic
-    @Column(name = "active", nullable = false)
-    private Boolean active;
-
-    @Basic
-    @JsonIgnore
-    @Column(name = "user_created")
-    private Integer userCreated;
-    
-    @Basic    
-    @JsonIgnore
-    @Column(name = "created")
-    private Timestamp created;
-
-    @Basic
-    @JsonIgnore
-    @Column(name = "user_deleted")
-    private Integer userDeleted;
-
-    @Basic
-    @JsonIgnore
-    @Column(name = "deleted")
-    private Timestamp deleted;
 }
