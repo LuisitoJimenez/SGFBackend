27bb78fca43b1f75879ed2abb0d62de1d0d022f2
Luis Jimenez <wichin@outlook.com>
2024-01-04 01:11:29 -0600

    Refactor UserRefereeEntity class

--- src/main/java/com/coatl/sac/entity/UserRefereeEntity.java
+++ src/main/java/com/coatl/sac/entity/UserRefereeEntity.java
@@ -1,15 +1,12 @@
 package com.coatl.sac.entity;
 
-import java.sql.Timestamp;
-
 import org.hibernate.annotations.Where;
+
+import com.coatl.sac.entity.base.BaseEntity;
 
 import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,14 +14,9 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "users_referees")
+@Table(name = "user_referee")
 @Where(clause = "deleted IS NULL")
-public class UserRefereeEntity {
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false, unique = true)
-    private Integer id;
+public class UserRefereeEntity extends BaseEntity{
 
     @Basic
     @Column(name = "user_Id", nullable = false)
@@ -34,19 +26,4 @@ public class UserRefereeEntity {
     @Column(name = "referee_Id", nullable = false)
     private Integer refereeId;
 
-    @Basic
-    @Column(name = "user_created", nullable = false)
-    private Integer userCreated;
-
-    @Basic
-    @Column(name = "created" )
-    private Timestamp created;
-
-    @Basic
-    @Column(name = "user_deleted")
-    private Integer userDeleted;
-
-    @Basic
-    @Column(name = "deleted")
-    private Timestamp deleted;
 }
