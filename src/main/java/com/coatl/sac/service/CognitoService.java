936435d0ee065d66dce78ac457510e9d8d95d52a
Luis Jimenez <wichin@outlook.com>
2024-01-05 03:11:47 -0600

    Update user authentication logic to use bcrypt hashing algorithm

--- src/main/java/com/coatl/sac/service/CognitoService.java
+++ src/main/java/com/coatl/sac/service/CognitoService.java
@@ -3,6 +3,7 @@ package com.coatl.sac.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,7 @@ import com.amazonaws.services.cognitoidp.model.AdminUserGlobalSignOutRequest;
 import com.amazonaws.services.cognitoidp.model.AdminUserGlobalSignOutResult;
 import com.amazonaws.services.cognitoidp.model.AdminCreateUserRequest;
 import com.amazonaws.services.cognitoidp.model.AttributeType;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClient;
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClientBuilder;
@@ -25,76 +27,130 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CognitoService {
 
-    private final Environment environment;
-    private final AWSCognitoIdentityProvider cognitoIdentityProvider;
+        private final Environment environment;
 
-    public AdminCreateUserResult createUser(String userName, String password, String first, String last,
-            String secondLast) {
+        private final AWSCognitoIdentityProvider cognitoIdentityProvider;
 
-        AdminCreateUserRequest createUserRequest = new AdminCreateUserRequest()
-                .withUserPoolId(environment.getProperty("aws.cognito.pool.userPoolId"))
-                .withUsername(userName)
-                .withTemporaryPassword(password)
-                .withUserAttributes(
-                        new AttributeType().withName("email").withValue(userName),
-                        new AttributeType().withName("name").withValue(first),
-                        new AttributeType().withName("custom:last").withValue(last),
-                        new AttributeType().withName("custom:second_last").withValue(secondLast));
+        public AdminCreateUserResult createUser(String userName, String password, String first, String last,
+                        String secondLast) {
 
-        AdminCreateUserResult createUserResult = cognitoIdentityProvider.adminCreateUser(createUserRequest);
+                AdminCreateUserRequest createUserRequest = new AdminCreateUserRequest()
+                                .withUserPoolId(environment.getProperty("aws.cognito.pool.userPoolId"))
+                                .withUsername(userName)
+                                .withTemporaryPassword(password)
+                                .withUserAttributes(
+                                                new AttributeType().withName("email").withValue(userName),
+                                                new AttributeType().withName("name").withValue(first),
+                                                new AttributeType().withName("custom:last").withValue(last),
+                                                new AttributeType().withName("custom:second_last")
+                                                                .withValue(secondLast));
 
-        return createUserResult;
+                AdminCreateUserResult createUserResult = cognitoIdentityProvider.adminCreateUser(createUserRequest);
 
-    }
+                return createUserResult;
 
-    public AdminUpdateUserAttributesResult updateCognitoUserAttributes(String cognitoUserName, String email,
-            String first, String last,
-            String secondLast) {
-        AWSCognitoIdentityProvider cognitoIdentityProvider = AWSCognitoIdentityProviderClientBuilder.defaultClient();
+        }
 
-        List<AttributeType> userAttributes = new ArrayList<>();
+        public AdminUpdateUserAttributesResult updateCognitoUserAttributes(String cognitoUserName, String email,
+                        String first, String last, String secondLast) {
+                System.out.println(":::REGION " + environment.getProperty("aws.cognito.common.region") + ":::");
 
-        userAttributes.add(new AttributeType().withName("email").withValue(email));
-        userAttributes.add(new AttributeType().withName("name").withValue(first));
-        userAttributes.add(new AttributeType().withName("custom:last").withValue(last));
-        userAttributes.add(new AttributeType().withName("custom:second_last").withValue(secondLast));
+                List<AttributeType> userAttributes = new ArrayList<>();
 
-        AdminUpdateUserAttributesRequest updateUserAttributesRequest = new AdminUpdateUserAttributesRequest()
-                .withUserPoolId(environment.getProperty("aws.cognito.pool.userPoolId"))
-                .withUsername(cognitoUserName)
-                .withUserAttributes(userAttributes);
+                userAttributes.add(new AttributeType().withName("email").withValue(email));
+                userAttributes.add(new AttributeType().withName("name").withValue(first));
+                userAttributes.add(new AttributeType().withName("custom:last").withValue(last));
+                userAttributes.add(new AttributeType().withName("custom:second_last").withValue(secondLast));
 
-        AdminUpdateUserAttributesResult updateUserAttributesResult = cognitoIdentityProvider
-                .adminUpdateUserAttributes(updateUserAttributesRequest);
+                AdminUpdateUserAttributesRequest updateUserAttributesRequest = new AdminUpdateUserAttributesRequest()
+                                .withUserPoolId(environment.getProperty("aws.cognito.pool.userPoolId"))
+                                .withUsername(cognitoUserName)
+                                .withUserAttributes(userAttributes);
 
-        return updateUserAttributesResult;
+                AdminUpdateUserAttributesResult updateUserAttributesResult = this.cognitoIdentityProvider
+                                .adminUpdateUserAttributes(updateUserAttributesRequest);
 
-    }
+                return updateUserAttributesResult;
+        }
 
-    public AdminDisableUserResult adminDisableCognitoUser(String cognitoUsername) {
-        AWSCognitoIdentityProvider cognitoIdentityProvider = AWSCognitoIdentityProviderClient.builder().build();
+        /*
+         * public AdminUpdateUserAttributesResult updateCognitoUserAttributes(String
+         * cognitoUserName, String email,
+         * String first, String last,
+         * String secondLast) {
+         * 
+         * System.out.println(":::REGION " +
+         * environment.getProperty("aws.cognito.common.region") + ":::");
+         * 
+         * AWSCognitoIdentityProvider cognitoIdentityProvider =
+         * AWSCognitoIdentityProviderClientBuilder.defaultClient();
+         * 
+         * List<AttributeType> userAttributes = new ArrayList<>();
+         * 
+         * userAttributes.add(new AttributeType().withName("email").withValue(email));
+         * userAttributes.add(new AttributeType().withName("name").withValue(first));
+         * userAttributes.add(new
+         * AttributeType().withName("custom:last").withValue(last));
+         * userAttributes.add(new
+         * AttributeType().withName("custom:second_last").withValue(secondLast));
+         * 
+         * 
+         * 
+         * AdminUpdateUserAttributesRequest updateUserAttributesRequest = new
+         * AdminUpdateUserAttributesRequest()
+         * .withUserPoolId(environment.getProperty("aws.cognito.pool.userPoolId"))
+         * .withUsername(cognitoUserName)
+         * .withUserAttributes(userAttributes);
+         * 
+         * AdminUpdateUserAttributesResult updateUserAttributesResult =
+         * cognitoIdentityProvider
+         * .adminUpdateUserAttributes(updateUserAttributesRequest);
+         * 
+         * return updateUserAttributesResult;
+         * 
+         * }
+         */
 
-        AdminDisableUserRequest disableUserRequest = new AdminDisableUserRequest()
-                .withUserPoolId(environment.getProperty("aws.cognito.pool.userPoolId"))
-                .withUsername(cognitoUsername);
+        public AdminDisableUserResult adminDisableCognitoUser(String cognitoUsername) {
+                AdminDisableUserRequest disableUserRequest = new AdminDisableUserRequest()
+                                .withUserPoolId(environment.getProperty("aws.cognito.pool.userPoolId"))
+                                .withUsername(cognitoUsername);
 
-        AdminDisableUserResult disableUserResult = cognitoIdentityProvider.adminDisableUser(disableUserRequest);
+                AdminDisableUserResult disableUserResult = this.cognitoIdentityProvider
+                                .adminDisableUser(disableUserRequest);
 
-        return disableUserResult;
+                return disableUserResult;
+        }
 
-    }
+        public AdminUserGlobalSignOutResult adminSignOutCognitoUser(String cognitoUsername) {
+                AdminUserGlobalSignOutRequest signOutRequest = new AdminUserGlobalSignOutRequest()
+                                .withUserPoolId(environment.getProperty("aws.cognito.pool.userPoolId"))
+                                .withUsername(cognitoUsername);
 
-    public AdminUserGlobalSignOutResult adminSignOutCognitoUser(String cognitoUsername) {
-        AWSCognitoIdentityProvider cognitoIdentityProvider = AWSCognitoIdentityProviderClient.builder().build();
+                AdminUserGlobalSignOutResult signOutResult = this.cognitoIdentityProvider
+                                .adminUserGlobalSignOut(signOutRequest);
 
-        AdminUserGlobalSignOutRequest signOutRequest = new AdminUserGlobalSignOutRequest()
-                .withUserPoolId(environment.getProperty("aws.cognito.pool.userPoolId"))
-                .withUsername(cognitoUsername);
+                return signOutResult;
+        }
 
-        AdminUserGlobalSignOutResult signOutResult = cognitoIdentityProvider.adminUserGlobalSignOut(signOutRequest);
-
-        return signOutResult;
-
-    }
+        /*
+         * public AdminUserGlobalSignOutResult adminSignOutCognitoUser(String
+         * cognitoUsername) {
+         * AWSCognitoIdentityProvider cognitoIdentityProvider =
+         * AWSCognitoIdentityProviderClient.builder().withRegion(Regions.US_WEST_2).
+         * build();
+         * 
+         * AdminUserGlobalSignOutRequest signOutRequest = new
+         * AdminUserGlobalSignOutRequest()
+         * .withUserPoolId(environment.getProperty("aws.cognito.pool.userPoolId"))
+         * .withUsername(cognitoUsername);
+         * 
+         * AdminUserGlobalSignOutResult signOutResult =
+         * cognitoIdentityProvider.adminUserGlobalSignOut(signOutRequest);
+         * 
+         * return signOutResult;
+         * 
+         * }
+         */
 
 }
