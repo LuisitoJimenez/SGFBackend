3bfc35d682afd6c77a4cda94a425addaf02201a7
Luis Jimenez <wichin@outlook.com>
2024-01-04 19:29:00 -0600

    Remove unused import statements in FieldController.java

--- src/main/java/com/coatl/sac/controller/FieldController.java
+++ src/main/java/com/coatl/sac/controller/FieldController.java
@@ -1,7 +1,5 @@
 package com.coatl.sac.controller;
 
-import java.util.Locale;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
