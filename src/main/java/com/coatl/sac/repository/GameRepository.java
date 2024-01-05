30282cf99d39c90f28d3bf938084203cac60e54f
Luis Jimenez <wichin@outlook.com>
2024-01-04 19:30:01 -0600

    Refactor GameRepository queries

--- src/main/java/com/coatl/sac/repository/GameRepository.java
+++ src/main/java/com/coatl/sac/repository/GameRepository.java
@@ -2,8 +2,7 @@ package com.coatl.sac.repository;
 
 import java.sql.Date;
 import java.sql.Time;
-//import java.time.LocalDate;
-//import java.time.LocalDateTime;
+
 import java.util.List;
 import java.util.Map;
 
@@ -17,66 +16,65 @@ public interface GameRepository extends JpaRepository<GameEntity, Integer> {
 
     boolean existsByGameDate(Date gameDate);
 
-    boolean existsByGameTimeAndDeletedIsNull(Time gameTime);
+    boolean existsByGameTimeAndDeletedAtIsNull(Time gameTime);
 
-    boolean existsByNameAndDeletedIsNull(String name);
+    boolean existsByNameAndDeletedAtIsNull(String name);
 
     List<GameEntity> findAll();
 
     @Query(value = """
-            SELECT
-                game.id,
-                game.name,
-                game.game_date,
-                game.game_time,
-                sub.name AS sub,
-                gender.name AS gender,
-                user.name AS referee
-            FROM games game
-                INNER JOIN games_subs gameSub ON game.id = gameSub.game_id AND gameSub.deleted IS NULL
-                INNER JOIN subs sub ON gameSub.sub_id = sub.id AND sub.deleted IS NULL
-                INNER JOIN games_genders gameGender ON game.id = gameGender.game_id AND gameGender.deleted IS NULL
-                INNER JOIN genders gender ON gameGender.gender_id = gender.id AND gender.deleted IS NULL
-                INNER JOIN games_referees gameReferee ON game.id = gameReferee.game_id AND gameReferee.deleted IS NULL
-                INNER JOIN referees referee ON gameReferee.referee_id = referee.id AND referee.deleted IS NULL
-                INNER JOIN users_referees userReferee ON referee.id = userReferee.referee_id AND userReferee.deleted IS NULL
-                INNER JOIN users user ON userReferee.user_id = user.id AND user.deleted IS NULL
-            WHERE game.deleted IS NULL
-                            """, nativeQuery = true)
+            SELECT gm.id,
+                   gm.name,
+                   gm.game_date,
+                   gm.game_time,
+                   s.name    AS sub,
+                   g.name AS gender,
+                   u.name   AS referee
+            FROM game gm
+                    INNER JOIN game_sub gs ON gm.id = gs.game_id AND gs.deleted_at IS NULL
+                    INNER JOIN sub s ON gs.sub_id = s.id AND s.deleted_at IS NULL
+                    INNER JOIN game_gender gg ON gm.id = gg.game_id AND gg.deleted_at IS NULL
+                    INNER JOIN gender g ON gg.gender_id = g.id AND g.deleted_at IS NULL
+                    INNER JOIN game_referee gr ON g.id = gr.game_id AND gr.deleted_at IS NULL
+                    INNER JOIN referee r ON gr.referee_id = r.id AND r.deleted_at IS NULL
+                    INNER JOIN user_referee ur ON r.id = ur.referee_id AND ur.deleted_at IS NULL
+                    INNER JOIN user u ON ur.user_id = u.id AND u.deleted_at IS NULL
+            WHERE gm.deleted_at IS NULL
+                    """, nativeQuery = true)
     List<Map<String, Object>> getGameList();
 
     @Query(value = """
-            SELECT
-                game.id,
-                game.name,
-                game.game_date AS gameDate,
-                game.game_time AS gameTime,
-                sub.id AS subId,
-                gender.id AS genderId,
-                referee.id AS refereeId
-            FROM games game
-                INNER JOIN games_subs gameSub ON game.id = gameSub.game_id AND gameSub.deleted IS NULL
-                INNER JOIN subs sub ON gameSub.sub_id = sub.id AND sub.deleted IS NULL
-                INNER JOIN games_genders gameGender ON game.id = gameGender.game_id AND gameGender.deleted IS NULL
-                INNER JOIN genders gender ON gameGender.gender_id = gender.id AND gender.deleted IS NULL
-                INNER JOIN games_referees gameReferee ON game.id = gameReferee.game_id AND gameReferee.deleted IS NULL
-                INNER JOIN referees referee ON gameReferee.referee_id = referee.id AND referee.deleted IS NULL
-            WHERE game.deleted IS NULL AND game.id = :pGameId
-                    """, nativeQuery = true)
+            SELECT gm.id,
+                   gm.name,
+                   gm.game_date AS gameDate,
+                   gm.game_time AS gameTime,
+                   sub.id       AS subId,
+                   g.id         AS genderId,
+                   r.id         AS refereeId
+            FROM game gm
+                    INNER JOIN game_sub gs ON gm.id = gs.game_id AND gs.deleted_at IS NULL
+                    INNER JOIN sub sub ON gs.sub_id = sub.id AND sub.deleted_at IS NULL
+                    INNER JOIN game_gender gg ON gm.id = gg.game_id AND gg.deleted_at IS NULL
+                    INNER JOIN gender g ON gg.gender_id = g.id AND g.deleted_at IS NULL
+                    INNER JOIN game_referee gr ON gm.id = gr.game_id AND gr.deleted_at IS NULL
+                    INNER JOIN referee r ON gr.referee_id = r.id AND r.deleted_at IS NULL
+            WHERE gm.deleted_at IS NULL
+              AND gm.id = :pGameId;
+                               """, nativeQuery = true)
     Map<String, Object> findGameById(@Param("pGameId") Integer gameId);
 
     @Query(value = """
-            SELECT
-                game.id AS gameId,
-                game.name AS gameName,
-                team.id AS teamId,
-                team.name AS teamName,
-                team.coach
-            FROM games game
-                LEFT JOIN games_teams gameTeam ON game.id = gameTeam.game_id
-                LEFT JOIN teams team ON gameTeam.team_id = team.id
-            WHERE game.id = :pGameId AND gameTeam.deleted IS NULL;
-                            """, nativeQuery = true)
+            SELECT g.id   AS gameId,
+                   g.name AS gameName,
+                   t.id   AS teamId,
+                   t.name AS teamName,
+                   t.coach
+            FROM game g
+                     LEFT JOIN game_team gt ON g.id = gt.game_id
+                     LEFT JOIN team t ON gt.team_id = t.id
+            WHERE g.id = :pGameId
+              AND gt.deleted_at IS NULL
+                    """, nativeQuery = true)
     List<Map<String, Object>> getGameTeams(@Param("pGameId") Integer gameId);
 
 }
