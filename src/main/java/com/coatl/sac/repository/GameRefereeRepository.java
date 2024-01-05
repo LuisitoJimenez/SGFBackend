81baba44781f67a0601f985f112b7aa79b194e1d
Luis Jimenez <wichin@outlook.com>
2024-01-04 19:29:56 -0600

    Update findByGameIdAndDeletedIsNull to findByGameIdAndDeletedAtIsNull in GameRefereeRepository

--- src/main/java/com/coatl/sac/repository/GameRefereeRepository.java
+++ src/main/java/com/coatl/sac/repository/GameRefereeRepository.java
@@ -12,5 +12,5 @@ public interface GameRefereeRepository extends JpaRepository<GameRefereeEntity, 
     
     Optional<GameRefereeEntity> findByGameId(Integer gameId);
 
-    Optional<GameRefereeEntity> findByGameIdAndDeletedIsNull(Integer gameId);
+    Optional<GameRefereeEntity> findByGameIdAndDeletedAtIsNull(Integer gameId);
 }
