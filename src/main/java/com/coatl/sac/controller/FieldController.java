ea7f6fd372a0ecda37d597e2f001dbf83a79132c
Luis Jimenez <wichin@outlook.com>
2024-01-05 23:05:54 -0600

    Add CRUD operations for FieldController

--- src/main/java/com/coatl/sac/controller/FieldController.java
+++ src/main/java/com/coatl/sac/controller/FieldController.java
@@ -1,5 +1,9 @@
 package com.coatl.sac.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,5 +32,27 @@ public class FieldController {
     ) {
         return fieldService.createField(fieldDTO);
     }
-    
+
+    @GetMapping("")
+    @Operation(summary = "Get all fields")
+    public WebServiceResponse getAllFields () {
+        return new WebServiceResponse(fieldService.getFieldsList());
+    }
+
+    @DeleteMapping("/{fieldId}")
+    @Operation(summary = "Delete field")
+    public WebServiceResponse deletedField (
+        @PathVariable Integer fieldId
+    ) {
+        return fieldService.deleteField(fieldId);
+    }
+
+    @PatchMapping("/{fieldId}")
+    @Operation(summary = "Update field")
+    public WebServiceResponse updateField (
+        @PathVariable Integer fieldId,
+        @RequestBody FieldDTO fieldDTO
+    ) {
+        return fieldService.updateField(fieldDTO, fieldId);
+    }
 }
