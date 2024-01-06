391c48ab7c6a582a81e6ada770570d71e1f2a57c
Luis Jimenez <wichin@outlook.com>
2024-01-05 23:06:01 -0600

    Add methods to get fields list, delete field, and update field

--- src/main/java/com/coatl/sac/service/FieldService.java
+++ src/main/java/com/coatl/sac/service/FieldService.java
@@ -1,6 +1,10 @@
 package com.coatl.sac.service;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -46,4 +50,42 @@ public class FieldService {
 
         return new WebServiceResponse(true, "Field created successfully");
     }
+
+    public List<Map<String, Object>> getFieldsList() {
+        return fieldRepository.getFieldsList();
+    }
+
+    @Transactional
+    public WebServiceResponse deleteField(Integer fieldId) {
+        Locale locale = LocaleContextHolder.getLocale();
+
+        FieldEntity field = fieldRepository.findById(fieldId).orElseThrow(() -> new RuntimeException("Field not found"));
+        field.setDeletedAt(Timestamp.valueOf(LocalDateTime.now()));
+        field.setDeletedBy(1);
+        fieldRepository.save(field);
+        return new WebServiceResponse(true, messageSource.getMessage("field.delete.success", null, locale));
+    }
+
+    @Transactional
+    public WebServiceResponse updateField(FieldDTO fieldDTO, Integer fieldId) {
+        Locale locale = LocaleContextHolder.getLocale();
+
+        try {
+            FieldEntity field = fieldRepository.findById(fieldId).orElseThrow(() -> new RuntimeException("Field not found"));
+            field.setName(fieldDTO.getName());
+            field.setPhone(fieldDTO.getPhone());
+            field.setEmail(fieldDTO.getEmail());
+            field.setAddress(new Address(fieldDTO.getStreet(), fieldDTO.getPostalCode(), fieldDTO.getMunicipality(),
+                fieldDTO.getTown(), fieldDTO.getState()));
+            field.setCapacity(fieldDTO.getCapacity());
+            fieldRepository.save(field);
+
+            return new WebServiceResponse(true, messageSource.getMessage("field.update.success", null, locale));
+
+        } catch (Exception e) {
+            
+            return new WebServiceResponse(false, messageSource.getMessage("field.update.error", null, locale));
+        }
+    }
+
 }
