0255e7396f3c076371e35a348c572adcbb9e8204
Luis Jimenez <wichin@outlook.com>
2024-01-07 02:18:04 -0600

    Add findByGameId and findByGameIdAndDeletedAtIsNull methods to FieldGameRepository

--- src/main/java/com/coatl/sac/repository/FieldGameRepository.java
+++ src/main/java/com/coatl/sac/repository/FieldGameRepository.java
@@ -1,5 +1,7 @@
 package com.coatl.sac.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,10 @@ import com.coatl.sac.entity.FieldGameEntity;
 
 @Repository
 public interface FieldGameRepository extends JpaRepository<FieldGameEntity, Integer> {
+
+    Optional<FieldGameEntity> findByGameId(Integer gameId);
+
+    Optional<FieldGameEntity> findByGameIdAndDeletedAtIsNull(Integer gameId);
+
     
 }
