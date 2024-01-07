dcbc74b2c38323bef2708c8d59abb88f1b8f75ef
Luis Jimenez <wichin@outlook.com>
2024-01-07 02:18:08 -0600

    Add new query method to FieldRepository

--- src/main/java/com/coatl/sac/repository/FieldRepository.java
+++ src/main/java/com/coatl/sac/repository/FieldRepository.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.coatl.sac.entity.FieldEntity;
@@ -17,7 +18,7 @@ public interface FieldRepository extends JpaRepository<FieldEntity, Integer> {
 
     @Query(value = """
             SELECT field.id,
-                   field.logo,
+                   JSON_UNQUOTE(JSON_EXTRACT(field.logo, '$[0].name')) AS logo,
                    field.name,
                    field.address,
                    field.phone,
@@ -27,5 +28,18 @@ public interface FieldRepository extends JpaRepository<FieldEntity, Integer> {
             WHERE field.deleted_at IS NULL
                         """, nativeQuery = true)
     List<Map<String, Object>> getFieldsList();
+
+    @Query(value = """
+            SELECT field.id,
+                   JSON_UNQUOTE(JSON_EXTRACT(field.logo, '$[0].name')) AS logo,
+                   field.name,
+                   field.address,
+                   field.phone,
+                   field.email,
+                   field.capacity
+            FROM field
+            WHERE field.id=:pfieldId AND field.deleted_at IS NULL
+                        """, nativeQuery = true)
+    Map<String, Object> getFieldById(@Param("pfieldId") Integer fieldId);
 
 }
